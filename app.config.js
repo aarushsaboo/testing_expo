e47e4cc224cb7e171f5ca49c35ed81104d7f0f98
Aarush Saboo <160993404+aarushsaboo@users.noreply.github.com>
2025-04-01 04:46:43 +0530

    the project ids in both app.config.js & app.json should match.

--- app.config.js
+++ app.config.js
@@ -49,7 +49,7 @@ export default {
         origin: false,
       },
       eas: {
-        projectId: "AIzaSyCiNMgc2NAV9scAmqwvaE170ZlwdXA-1Xk",
+        projectId: "ff78d964-4d16-4b1e-9ecc-a0daddc6d26e",
       },
       // Add your environment variables here
       geminiApiKey: envVars.GEMINI_API_KEY || "FALLBACK_API_KEY",
